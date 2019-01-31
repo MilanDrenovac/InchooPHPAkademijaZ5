9ac0ead1f28f63dd103d0b48bb34f887263631bc
Mew <milandrenovac@gmail.com>
2019-01-31 19:21:32 +0100

    Predavanje

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -10,4 +10,33 @@ class IndexController
             "posts" => $posts
         ]);
     }
+    public function newPost()
+    {
+        $data = $this->validate($_POST);
+        if ($data === false){
+            header('Location:'.App::config('url'));
+        }
+        else{
+            $connection = Db::connect();
+            $sql = 'insert into post (content) values (:content)';
+            $statement = $connection->prepare($sql);
+            $statement->bindValue('content',$data['content']);
+            $statement->execute();
+            header('Location:'.App::config('url'));
+        }
+    }
+    private function validate($data)
+    {
+        $required = ['content'];
+        foreach ($required as $key){
+            if(!isset($data[$key])){
+                return false;
+            }
+            $data[$key] = trim((string)$data[$key]);
+            if (empty($data[$key])){
+                return false;
+            }
+            return $data;
+        }
+    }
 }
